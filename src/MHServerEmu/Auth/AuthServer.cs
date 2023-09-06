fffdd8bc58f321060248b4a5cfc48f2ca6835d82
DrunkenMonk <chris@consolemod.co.uk>
2023-09-06 20:52:39 +0100

    public auth

--- src/MHServerEmu/Auth/AuthServer.cs
+++ src/MHServerEmu/Auth/AuthServer.cs
@@ -20,7 +20,7 @@ namespace MHServerEmu.Auth
 
         public AuthServer(int port, FrontendService frontendService)
         {
-            _url = $"http://192.168.1.9:{port}/";
+            _url = $"http://mh.thestreamstation.com:{port}/";
             _frontendService = frontendService;
             _cancellationTokenSource = new();
         }
