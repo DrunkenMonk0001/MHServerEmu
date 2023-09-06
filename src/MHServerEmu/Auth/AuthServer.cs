d3dbaef7eec6cae69dd822d1be477c1f70fdc2bd
DrunkenMonk <chris@consolemod.co.uk>
2023-09-06 21:18:58 +0100

    nope

--- src/MHServerEmu/Auth/AuthServer.cs
+++ src/MHServerEmu/Auth/AuthServer.cs
@@ -21,7 +21,7 @@ namespace MHServerEmu.Auth
 
         public AuthServer(int port, FrontendService frontendService)
         {
-            _url = $"http://192.168.1.9:{port}/";
+            _url = $"http://193.237.201.24:{port}/";
             _frontendService = frontendService;
             _cancellationTokenSource = new();
         }
